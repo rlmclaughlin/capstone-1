89501c61e00452a9bd57e9ee271bc8feaa1c2a1c
rlmclaughlin <rlmclaughlin@yahoo.com>
2021-02-08 11:32:38 -0600

    added login form and register form boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import Nav from './components/nav/nav'
 import InventoryCards from './components/inventory/inventoryCards'
+import Login from './components/forms/login'
+import Register from './components/forms/register'
 
 import './App.css';
 import {Route} from 'react-router-dom'
@@ -7,12 +9,18 @@ import {Route} from 'react-router-dom'
 function App() {
   const inventoryData = require('./data/inventory.json')
   const usersData = require('./data/users.json')
-  console.log(usersData)
+
   return (
     <div className="App">
       <Nav/>
       <Route exact path='/' render={props => (
         <InventoryCards {...props} inventoryData={inventoryData}/>
+      )}/>
+      <Route path='/login' render={props => (
+        <Login {...props} usersData={usersData}/>
+      )}/>
+      <Route path='/register' render={props => (
+        <Register {...props} usersData={usersData}/>
       )}/>
     </div>
   );
---
+++ client/src/components/forms/login.jsx
@@ -0,0 +1,31 @@
+import React, {useState} from 'react'
+
+function Login(){
+    const [user, setUser] = useState({username: '', email: '', password: ''})
+
+    const changeHandler = () => {
+
+    }
+    
+    return(
+        <form>
+            <input name='username' 
+                   type='text'
+                   placeholder='username'
+                   value={user.username}
+                   onChange={changeHandler}/>
+            <input name='email' 
+                   type='text'
+                   placeholder='email'
+                   value={user.email}
+                   onChange={changeHandler}/>
+            <input name='password' 
+                   type='text'
+                   placeholder='password'
+                   value={user.password}
+                   onChange={changeHandler}/>
+        </form>
+    )
+}
+
+export default Login
