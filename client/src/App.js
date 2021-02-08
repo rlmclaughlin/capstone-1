833d892b6c5a1b7c9049952b8925d867d1376e6f
rlmclaughlin <rlmclaughlin@yahoo.com>
2021-02-08 10:57:39 -0600

    created dummy data and it's communicating with app.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,10 @@ import InventoryCards from './components/inventory/inventoryCards'
 import './App.css';
 import {Route} from 'react-router-dom'
 
+const myData = require('./data/data.json')
+
 function App() {
+  console.log(myData)
   return (
     <div className="App">
       <Nav/>
